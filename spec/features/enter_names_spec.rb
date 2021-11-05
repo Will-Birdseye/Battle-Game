7f6c63a4a03997511d147cbdba49d2f261a25d5e
Will <w_birdseye@hotmail.co.uk>
2021-11-05 11:46:49 +0000

    Attack button added with response and feature test

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Teffox'
-    fill_in :player_2_name, with: 'Deimos'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Teffox vs. Deimos'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'see Player 2 hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Teffox'
-    fill_in :player_2_name, with: 'Deimos'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Deimos: 60HP'
   end
 end
